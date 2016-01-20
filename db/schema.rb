97514f4de8d3d7d8b1b9d082d612756cc6c346cd
wesly-mezarina <wesly@mezarina.me>
2016-01-20 15:09:57 -0800

    Generated model Genre

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20160120230423) do
+ActiveRecord::Schema.define(version: 20160120230859) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -22,6 +22,12 @@ ActiveRecord::Schema.define(version: 20160120230423) do
     t.date     "aired_on"
     t.date     "ended_on"
     t.string   "duration"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+  end
+
+  create_table "genres", force: :cascade do |t|
+    t.string   "name"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
   end
