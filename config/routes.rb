3da0b074bb39ccbebd7dab5a8f24829d374380c8
wesly-mezarina <wesly@mezarina.me>
2016-01-29 15:19:44 -0800

    Add a navbar in application.html.erb and add a sign in and sign out button

--- config/routes.rb
+++ config/routes.rb
@@ -12,6 +12,7 @@ Rails.application.routes.draw do
 
   devise_for :users
   resources :users, only: [:show]
+  resources :animes
   # The priority is based upon order of creation: first created -> highest priority.
   # See how all your routes lay out with "rake routes".
 
