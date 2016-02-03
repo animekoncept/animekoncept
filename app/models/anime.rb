b10532e8e7f07dbbed4a73c9d3f270edef3835ef
wesly-mezarina <wesly@mezarina.me>
2016-02-03 15:03:49 -0800

    Added Zurb Foundation interchange to header_image

--- app/models/anime.rb
+++ app/models/anime.rb
@@ -42,6 +42,6 @@
   has_attached_file :cover_image, styles: { large: "200x288#", medium: "162x230>", thumb: "100x100>" }, default_url: "/images/:style/missing.png"
   validates_attachment_content_type :cover_image, content_type: /\Aimage\/.*\Z/
 
-  has_attached_file :header_image, styles: { large: "1920x500#" }, default_url: "/images/:style/missing.png"
+  has_attached_file :header_image, styles: { large: "1920x600#", medium: "1920x800#" }, default_url: "/images/:style/missing.png"
   validates_attachment_content_type :header_image, content_type: /\Aimage\/.*\Z/
 end
