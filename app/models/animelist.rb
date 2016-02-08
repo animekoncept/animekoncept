48713ca0e577e62161f53b5c4b15fd340e1956ea
wesly-mezarina <wesly@mezarina.me>
2016-02-08 12:27:13 -0800

    Add a link_to on anime name

--- app/models/animelist.rb
+++ app/models/animelist.rb
@@ -4,11 +4,4 @@ class Animelist < ActiveRecord::Base
 
   STATUSES = ["Watching", "Plan to Watch", "Completed", "On Hold", "Dropped" ]
   validates :status, inclusion: {in: STATUSES}
-
-  validate :rating_is_valid
-  def rating_is_valid
-    if self.rating && (self.rating <= 0 || self.rating > 5 || (self.rating * 2) % 1 != 0)
-      errors.add(:rating, "is not in the valid range")
-    end
-  end
 end
