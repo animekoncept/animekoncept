7c1365d642897a07fc45d8904230e01cbe59b251
wesly-mezarina <wesly@mezarina.me>
2016-04-13 14:04:49 -0700

    Each category has a specific color label

--- app/models/category.rb
+++ app/models/category.rb
@@ -1,3 +1,26 @@
 class Category < ActiveRecord::Base
   has_many :topics
+
+  def label_class
+    label_class_map[title]
+  end
+
+  private
+    def label_class_map
+      {
+        'site news' => 'alert',
+        'industry news' => 'industry_news',
+        'general' => 'general',
+        'anime' => 'anime',
+        'manga' => 'manga',
+        'visual novels' => 'visual_novels',
+        'feedback' => 'feedback',
+        'events' => 'events',
+        'gaming' => 'gaming',
+        'music' => 'music',
+        'recommendation' => 'recommendation',
+        'dev' => 'dev',
+        'creations' => 'creations',
+      }
+    end
 end
