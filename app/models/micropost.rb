e17df640ad5714a40b212dbc659f3312965812ac
wesly-mezarina <wesly@mezarina.me>
2016-04-27 20:33:45 -0700

    Set validation on content length

--- app/models/micropost.rb
+++ app/models/micropost.rb
@@ -1,5 +1,6 @@
 class Micropost < ActiveRecord::Base
   validates_presence_of :content
+  validates_length_of :content, maximum: 140, message: "less than 140 if you don't mind"
   belongs_to :user
   default_scope ->{ order(created_at: :desc)}
   markable_as :like
