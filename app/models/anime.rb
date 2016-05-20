01756539cff86389ee4c6caf75f930361ffd1c96
wesly-mezarina <wesly@mezarina.me>
2016-05-20 16:45:25 -0700

    Un-comment synopsis and aired_on validation

--- app/models/anime.rb
+++ app/models/anime.rb
@@ -24,8 +24,8 @@
   #tracked owner: ->{ |controller, model| controller.current_user }
 
   validates_presence_of :name
-  #validates_presence_of :synopsis
-  #validates_presence_of :aired_on
+  validates_presence_of :synopsis
+  validates_presence_of :aired_on
   #validates_presence_of :ended_on
   #validates_presence_of :duration
   validates_uniqueness_of :name, :case_sensitive => false
