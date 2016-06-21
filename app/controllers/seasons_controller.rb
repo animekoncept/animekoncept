0e68bb5d2e8ccda435dfa5718685e6e82073b69a
wesly-mezarina <wesly@mezarina.me>
2016-06-20 18:56:25 -0700

    Deleted everything in seasons/show.html.erb and add a render page

--- app/controllers/seasons_controller.rb
+++ app/controllers/seasons_controller.rb
@@ -2,7 +2,8 @@ class SeasonsController < ApplicationController
 
   def show
     @season = Season.find params[:id]
-    @animes = @season.animes
+    @animes = @season.animes.limit(2)
+    @all_animes = @animes.where.not(id: @animes.map(&:id)).page(params[:page]).per(42)
     @seasons = Season.all.limit(4)
     @genres = Genre.all
   end
