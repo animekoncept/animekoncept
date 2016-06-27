afa172e5580b7af33d691f846a34fb66ab12ad28
wesly-mezarina <wesly@mezarina.me>
2016-06-27 08:37:49 -0700

    Comment out aws code

--- config/environments/development.rb
+++ config/environments/development.rb
@@ -5,12 +5,12 @@ Rails.application.configure do
   # every request. This slows down response time but is perfect for development
   # since you don't have to restart the web server when you make code changes.
 
-  config.paperclip_defaults = {
-    :storage => :s3,
-    :url =>':s3_domain_url',
-    :bucket => "chibichibi",
-    :path => '/:class/:attachment/:id_partition/:style/:filename'
-  }
+  #config.paperclip_defaults = {
+  #  :storage => :s3,
+  #  :url =>':s3_domain_url',
+  #  :bucket => "chibichibi",
+  #  :path => '/:class/:attachment/:id_partition/:style/:filename'
+  #}
 
 
   config.cache_classes = false
