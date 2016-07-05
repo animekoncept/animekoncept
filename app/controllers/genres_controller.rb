a86b1a1c5d9317bf595c53c8bebb04354b152b98
wesly-mezarina <wesly@mezarina.me>
2016-07-05 12:22:12 -0700

    Rendered partial page in genres

--- app/controllers/genres_controller.rb
+++ app/controllers/genres_controller.rb
@@ -3,6 +3,7 @@ class GenresController < ApplicationController
   def show
     @genre = Genre.find params[:id]
     @genres = Genre.all.order(:title)
-    @animes = @genre.animes
+    @animes = @genre.animes.limit(2)
+    @all_animes = @animes.where.not(id: @animes.map(&:id)).page(params[:page]).per(42)
   end
 end
