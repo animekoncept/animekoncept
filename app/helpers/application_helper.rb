eadd97c97048b23b353598062e36cc5d0a79b450
wesly-mezarina <wesly@mezarina.me>
2016-08-17 16:24:16 -0700

    Add greeting message before username

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -1,13 +1,5 @@
 module ApplicationHelper
 
-  def username
-    if user_signed_in?
-      current_user.username
-    else
-      "Guest"
-    end
-  end
-
   def markdown(text)
     markdown = Redcarpet::Markdown.new(Redcarpet::Render::HTML,
                                          :hard_wrap => true,
@@ -52,6 +44,32 @@ module ApplicationHelper
 
   def active_link(link_path)
     current_page?(link_path) ? "active_link" : ""
+  end
+
+  def username
+    if user_signed_in?
+      current_user.username
+    else
+      "Guest"
+    end
+  end
+
+  def greet
+    current_time = Time.now.to_i
+    midnight = Time.now.beginning_of_day.to_i
+    noon = Time.now.middle_of_day.to_i
+    five_pm = Time.now.change(:hour => 17 ).to_i
+    eight_pm = Time.now.change(:hour => 20 ).to_i
+
+    if midnight.upto(noon).include?(current_time)
+      "Good Morning"
+    elsif noon.upto(five_pm).include?(current_time)
+      "Good Afternoon"
+    elsif five_pm.upto(eight_pm).include?(current_time)
+      "Good Evening"
+    elsif eight_pm.upto(midnight + 1.day).include?(current_time)
+      "Good Night"
+    end
   end
 
 end
