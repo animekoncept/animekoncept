095ff0ad4b141a88fa100c10d1e17f8d897f08ea
wesly-mezarina <wesly@mezarina.me>
2016-09-15 20:29:46 -0700

    Set consider_all_requests_local to true

--- config/application.rb
+++ config/application.rb
@@ -12,7 +12,6 @@ module Testing
   class Application < Rails::Application
     config.assets.paths << Rails.root.join("app", "assets", "fonts")
     config.assets.image_optim = false
-    config.assets.initialize_on_precompile = false
     # Settings in config/environments/* take precedence over those specified here.
     # Application configuration should go into files in config/initializers
     # -- all .rb files in that directory are automatically loaded.
--- config/environments/production.rb
+++ config/environments/production.rb
@@ -9,9 +9,9 @@ Rails.application.configure do
   # and those relying on copy on write to perform better.
   # Rake tasks automatically ignore this option for performance.
   config.eager_load = true
-  config.serve_static_assets = true
+
   # Full error reports are disabled and caching is turned on.
-  config.consider_all_requests_local       = false
+  config.consider_all_requests_local       = true
   config.action_controller.perform_caching = true
 
   # Enable Rack::Cache to put a simple HTTP cache in front of your application
