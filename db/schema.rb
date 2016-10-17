30a2e14e19c099ac573cf18ec46b841172ed95b6
wesly-mezarina <wesly@mezarina.me>
2016-10-16 20:35:02 -0700

    Run migration to add images to groups

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20160803035737) do
+ActiveRecord::Schema.define(version: 20161017033207) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -140,6 +140,23 @@ ActiveRecord::Schema.define(version: 20160803035737) do
   end
 
   add_index "genres", ["slug"], name: "index_genres_on_slug", unique: true, using: :btree
+
+  create_table "groups", force: :cascade do |t|
+    t.string   "title"
+    t.text     "bio"
+    t.text     "summary"
+    t.integer  "user_id"
+    t.datetime "created_at",                null: false
+    t.datetime "updated_at",                null: false
+    t.string   "avatar_file_name"
+    t.string   "avatar_content_type"
+    t.integer  "avatar_file_size"
+    t.datetime "avatar_updated_at"
+    t.string   "header_image_file_name"
+    t.string   "header_image_content_type"
+    t.integer  "header_image_file_size"
+    t.datetime "header_image_updated_at"
+  end
 
   create_table "marks", force: :cascade do |t|
     t.integer  "marker_id"
