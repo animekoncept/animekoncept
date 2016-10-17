39ee49b4477930b1f03aafcb66bb44b50f0095b6
wesly-mezarina <wesly@mezarina.me>
2016-10-16 20:56:13 -0700

    Add validations and attachments to images

--- app/models/group.rb
+++ app/models/group.rb
@@ -1,2 +1,23 @@
 class Group < ActiveRecord::Base
+  extend FriendlyId
+  friendly_id :title, use: :slugged
+
+  validates_presence_of :title
+  validates_uniqueness_of :title
+
+  validates_presence_of :bio
+  validates_presence_of :summary
+
+  belongs_to :user
+
+
+  has_attached_file :avatar,
+                    styles: { large: "200x288#", medium: "162x230#", thumb: "100x100#", mobile: "350x444" },
+                    processors: [:thumbnail, :paperclip_optimizer]
+  validates_attachment_content_type :avatar, content_type: /\Aimage\/.*\Z/
+
+  has_attached_file :header_image,
+                    styles: { large: "1920x600#", medium: "1920x850#" },
+                    processors: [:thumbnail, :paperclip_optimizer]
+  validates_attachment_content_type :header_image, content_type: /\Aimage\/.*\Z/
 end
