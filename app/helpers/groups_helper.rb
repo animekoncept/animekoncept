dd2e37d23e76e55039f6faa4d4146a4c5206264f
wesly-mezarina <wesly@mezarina.me>
2016-11-20 15:42:01 -0800

    Add helper method to only show comment form if user joined the group

--- app/helpers/groups_helper.rb
+++ app/helpers/groups_helper.rb
@@ -1,2 +1,9 @@
 module GroupsHelper
+
+  def joined_user
+    if @group.marked_as_join? by: current_user
+      render partial: "comments/template", locals: {commentable: @group, new_comment: @comment} if user_signed_in?
+    else
+    end
+  end
 end
